15491145d6ef5c01d8cce3e0d097bcf76e8ef5f7
Alex Fall <alex@fallalex.com>
2020-08-30 18:14:11 -0400

    chose the return for status

--- status.py
+++ status.py
@@ -1,26 +1,55 @@
 #!/usr/bin/python3
 from mcstatus import MinecraftServer
 import  psutil
- 
-mc = MinecraftServer('localhost',25565)
+from time import time
+import libtmux
 
-mc.ping()
+class mcStatus:
+    def __init__(self):
+        self.mc = MinecraftServer('localhost',25565)
+        self.username = "mc"
+        self.process_name = "java"
+        self.cmd_query = "server.jar"
+        self.stime = None
 
-def find_proc(user, name, search):
-    ps = []
-    for p in psutil.process_iter(["username","name","cmdline","pid"]):
-        if user == p.info['username']:
-            if name == p.info['name']:
-                if search in p.info['cmdline']:
-                    ps.append(p.info['pid'])
-    if len(ps) == 0:
-        return None
-    elif len(ps) == 1:
-        return ps[0]
-    # to define custom Exception need class
-    raise Exception('More than one server process found PIDs: {}'.format(str(ps)))
+    def ping(self):
+        try:
+            self.mc.ping()
+            return True
+        except:
+            return False
 
-print(find_proc("mc", "java", "server.jar"))
+    def find_proc(self):
+        ps = []
+        for p in psutil.process_iter(["username","name","cmdline","pid","create_time"]):
+            if self.username == p.info['username']:
+                if self.process_name == p.info['name']:
+                    if self.cmd_query in p.info['cmdline']:
+                        ps.append((p.info['pid'], p.info['create_time']))
+        if len(ps) == 0: return None
+        elif len(ps) == 1:
+            self.stime = int(time() - ps[0][1])
+            return ps[0][0]
+        # to define custom Exception need class
+        raise Exception('More than one server process found PIDs: {}'.format(str(ps)))
+
+    def tmux(self):
+
+
+    
+def main():
+    mc = mcStatus()
+    pid = mc.find_proc()
+    mc.tmux()
+    if pid == None:
+        return 1
+    if mc.ping():
+        print(mc.stime)
+        return 0
+    return 2
+
+if __name__ == "__main__":
+    main()
 
 """
 Can only be in these states:
@@ -29,8 +58,25 @@ Can only be in these states:
   - up
     - mcstatus
   - starting (wait)
+    - pid but mcstatus does not return
   - stuck/error
     - no mcstatus
     - timeout
     - expected tmux window not found
+"""
+  
+"""
+status
+0 up 
+1 down
+
+2 starting
+
+check for pid
+if not pid:
+    return 1
+try mcstatus
+    return 0
+except
+    return 2
 """
